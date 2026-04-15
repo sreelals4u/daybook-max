e63b5143738723c270086a11b9db48111d571bd5
sreelals4u <sreelal.s4u@gmail.com>
2026-04-15 16:04:22 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'daybook-max-v3';
+const urlsToCache = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+});
